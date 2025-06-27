620e9dc898ca5dd77fe8df2afb4f77ea77f8b488
Mike James <mike.james@falcondev.tech>
2025-06-27 13:10:26 -0300

    Modified logging to only print the syslog message once no matter what parser is used

--- fsyslog.py
+++ fsyslog.py
@@ -73,7 +73,6 @@ class Fsyslog():
 
     def parse_arbor(self, message):
 # Arbor  <125>Oct  9 15:35:10 pfsp: TMS mitigation 'Alert 10696181 IPv4 Auto-Mitigation' started at 2024-10-09 09:35:09 CST, leader arbui2.opentransit.net, managed object 'ITELLUM' (5729), first diversion prefix 190.61.60.251/32
-        logging.info(f'Message {message}')
         m = arbor_tms_mitigation_regex.match(message)
         if m:
             logging.info('Arbor TMS Mitigation message')
@@ -95,7 +94,6 @@ class Fsyslog():
     def parse_fortigate(self, message):
 # <189>1 2025-06-04T15:29:06Z FortiGate-1800F-GUA - - - - eventtime=1749050946187327342 tz="-0600" logid="0000000013" type="traffic" subtype="forward" level="notice" vd="root" srcip=2803:1040:1802:3b4b:7ca4:6cd6:e0de:714d srcport=58769 srcintf="port36" srcintfrole="wan" dstip=2803:e880:8111:1b:: dstport=53 dstintf="ZONA-DNS" dstintfrole="lan" sessionid=395601599 proto=17 action="accept" policyid=430 policytype="policy" poluuid="bd93d5a2-0bdf-51ef-d488-97112a346de5" policyname="SGACTNICASAIPV6" srccountry="Nicaragua" dstcountry="Guatemala" service="DNS" trandisp="noop" duration=30 sentbyte=92 rcvdbyte=197 sentpkt=1 rcvdpkt=1 appcat="unscanned" dsthwvendor="HP" masterdstmac="94:18:82:68:22:35" dstmac="94:18:82:68:22:35" dstserver=1
         message = str(message)
-        logging.info(f'Message {message}')
         d = {}
         start_str = '- - - -'
         pos = message.find(start_str)
@@ -115,6 +113,8 @@ class Fsyslog():
     def parse(self, message):
 
         json_payload = self.parse_arbor(message)
+        logging.info(f'Message {message}')
+
         if not(json_payload):
             json_payload = self.parse_kentik(message)
         if not(json_payload):
